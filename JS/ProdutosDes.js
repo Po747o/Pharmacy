2c5ce901ad6c26bec0e801a6fa65c8720dd36115
Samara2006 <83661958+Samara-maker@users.noreply.github.com>
2023-10-20 14:28:13 -0400

    att

---
+++ JS/ProdutosDes.js
@@ -0,0 +1,32 @@
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCarWhidth = carousel.querySelector(".card").offsetwidth;
+
+let isDragging = false, startX, startScrollLeft;
+
+arrowBtns.forEach(btn=>{
+    btn.addEventListener("click", ()=> {
+        carousel.scrollLeft += btn.id === "left" ? -firstCarWhidth : firstCarWhidth;
+    })
+})
+
+const dragStart = (e) =>{
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    console.scrollLeft = startScrollLeft - (e.pageX - startX);//barra de rolagem
+}
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
